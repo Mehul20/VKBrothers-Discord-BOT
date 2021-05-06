3a41d31606ce359972fb908b05a70cfa8b78b4dd
Mehul <mehulrastogi2002@gmail.com>
2021-05-07 02:14:40 +0530

    POLL added and changed

--- bot.js
+++ bot.js
@@ -3,13 +3,41 @@ const client = new Discord.Client({
     partials: ["MESSAGE"]
 });
 require("dotenv").config()
+const pollEmbed = require('discord.js-poll-embed');
 
 client.on('ready', () => {
   console.log("BOT Ready to GO!!!");
 });
+
+
+var cars = ["Account Opened",
+ "Invoice Created", 
+ "Delivered",
+"Some Problems"];
+
+const defEmojiList = [
+	'\u0031\u20E3',
+	'\u0032\u20E3',
+	'\u0033\u20E3',
+	'\u0034\u20E3',
+	'\u0035\u20E3',
+	'\u0036\u20E3',
+	'\u0037\u20E3',
+	'\u0038\u20E3',
+	'\u0039\u20E3',
+	'\uD83D\uDD1F'
+];
+
+
 client.on('message', msg => {
     if (msg.content.includes("!order")) {
-      msg.reply('Pong!');
+      msg.reply('New Order has come, VK Brothers BOT will track the progress!');
+      var okay = pollEmbed(msg,"Status of Order",cars,0,defEmojiList,'\u274C');
+      okay.then(function () {
+        console.log("Promise Resolved");
+   }).catch(function () {
+        console.log("Promise Rejected");
+   });
     }
   });
 
@@ -18,3 +46,5 @@ client.on('messageDelete', msg => {
 });
 
 client.login(process.env.BOT_TOKEN);
+
+
